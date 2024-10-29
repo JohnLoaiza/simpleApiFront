6174010fd60ab64a774cb21afbea2474b1e3197b
JohnLoaiza <jhon91811@gmail.com>
2024-10-29 14:55:32 -0500

    Procesado al crear nuevas entidades de tablas que tengan objetos como parametros o listados de strings

--- src/components/editModal.tsx
+++ src/components/editModal.tsx
@@ -13,6 +13,7 @@ interface modalProps {
 const EditModal = (props: modalProps) => {
     const { doc, onClose, indexList, editDoc, asEdit = false, addDoc } = props;
     const [document, setDocument] = useState<any>();
+    const [objectPropieties, setObjectPropieties] = useState({})
 
     const submitEdit = () => {
         console.log(document.propierties); editDoc(document.id, document); onClose()
@@ -26,23 +27,44 @@ const EditModal = (props: modalProps) => {
         setDocument(document);
     }
 
+    const changeAsObject = (e: React.ChangeEvent<HTMLInputElement>, property: string, i: string) => {
+
+        try {
+            document.propierties[property][i] = e.target.value;
+        } catch (error) {
+           document.propierties[property] = {
+            ...document.propierties[property], // Copiamos el objeto anterior
+            [i]: e.target.value, // Agregamos el nuevo par clave-valor
+          }
+        }
+        
+        setDocument(document);
+        console.log('documento se va viendo');
+        console.log(document);
+        
+        
+    }
+
+    const changeAsArray = (e: React.ChangeEvent<HTMLInputElement>, i: string) => {
+        try {
+            document.propierties[i][0] = e.target.value;
+        } catch (error) {
+            document.propierties[i] = []
+
+            document.propierties[i].push(e.target.value);
+
+        }
+        
+        setDocument(document);
+    }
+
     useEffect(() => {
         asEdit ? setDocument(doc) : setDocument({ id: 0, propierties: {} })
         return () => { };
     }, []);
 
-    const objectForm = (finalValue: any, key: string) => {
 
-
-        return <>
-        <div>
-            <label>{key + ': '}</label>
-            {objetcToForm(Object.keys(finalValue), finalValue)}
-        </div>
-    </>
-    }
-
-    const objetcToForm = (indexList: string[], objetc: any) => {
+    const objetcToForm = (indexList: string[], objetc: any, isPrioperty?: boolean, propertyName?: string) => {
         var finalList: JSX.Element[] = [];
         indexList.map((i) => {
             var finalValue;
@@ -59,36 +81,39 @@ const EditModal = (props: modalProps) => {
                 console.log(finalValue);
                 console.log('indexes son');
                 console.log(Object.keys(finalValue));
-                render = objectForm(finalValue, i);
+                render = <>
+                <div>
+                    <label>{i + ': '}</label>
+                    {objetcToForm(Object.keys(finalValue), finalValue, true,i)}
+                </div>
+            </>;
 
             } else if (Array.isArray(jsonParse)) {
                 finalValue = jsonParse
-                render = isObject(finalValue[0]) ? (asEdit? renderObjectList(finalValue) :<>{objetcToForm(Object.keys(finalValue[0]), finalValue)}</> ): (asEdit?
-                 <>
-                <strong> {'Lista de ' + i}</strong>   
-                <ul>
-                    {finalValue.map((e: any) => {
-                        return <>
+                render = isObject(finalValue[0]) ? (asEdit ? renderObjectList(finalValue) : <><strong>{i}</strong>{objetcToForm(Object.keys(finalValue[0]), finalValue, true, i)}</>) : (asEdit ?
+                    <>
+                        <strong> {'Lista de ' + i}</strong>
+                        <ul>
+                            {finalValue.map((e: any) => {
+                                return <>
 
-                            {<li>{e}</li> }
-                        </>
-                    })}
-                    </ul>
-                </>: <label>
-                    <div style={{ width: '10%' }}>{i}</div>
-                    <div style={{ width: '10%' }}> <input type="text" value={undefined} onChange={(e) => change(e, i)} /></div>
-                </label>
-                
-            )
-            }
+                                    {<li>{e}</li>}
+                                </>
+                            })}
+                        </ul>
+                    </> : <label>
+                        <div style={{ width: '10%' }}>{i}</div>
+                        <div style={{ width: '10%' }}> <input type="text" value={undefined} onChange={(e) => changeAsArray(e, i)} /></div>
+                    </label>
 
-            else {
+                )
+            } else {
                 finalValue = objetc[i];
                 console.log('pasa derecho ');
                 console.log(finalValue);
                 render = <label>
                     <div style={{ width: '10%' }}>{i}</div>
-                    <div style={{ width: '10%' }}> <input type="text" value={document && asEdit ? finalValue : undefined} onChange={(e) => change(e, i)} /></div>
+                    <div style={{ width: '10%' }}> <input type="text" value={document && asEdit ? finalValue : undefined} onChange={(e) => isPrioperty ? changeAsObject(e,propertyName!, i) : change(e, i)} /></div>
                 </label>
             }
 
