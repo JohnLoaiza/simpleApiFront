fb8eeb0dbf9ff3b21c9f4d492c1372b94d9bb72b
JohnLoaiza <jhon91811@gmail.com>
2024-11-18 10:12:20 -0500

    se controla el agregar nuevos documentos que tengan sub documentos y se deja de mostrar los subdocumentos al editar un documentos, solo se editan sus propiedades que no sean objetos o arrays y estos se editan en su propia tabla
    jl

--- src/components/editModal.tsx
+++ src/components/editModal.tsx
@@ -7,7 +7,7 @@ interface modalProps {
     editDoc: (id: string, doc: any) => void;
     indexList: string[];
     asEdit: boolean
-    addDoc: ( doc: any) => void,
+    addDoc: (doc: any) => void,
     obj: any,
     mapList: string[],
     indexEdit: number,
@@ -15,7 +15,7 @@ interface modalProps {
 }
 
 const EditModal = (props: modalProps) => {
-    const {editAs, mapList,indexEdit, obj, doc, onClose, indexList, editDoc, asEdit = false, addDoc } = props;
+    const { editAs, mapList, indexEdit, obj, doc, onClose, indexList, editDoc, asEdit = false, addDoc } = props;
     const [objeto, setObject] = useState<any>();
     const [edit, setEdit] = useState(false)
 
@@ -23,14 +23,14 @@ const EditModal = (props: modalProps) => {
         var collectionUpdate = doc.propierties
         console.log('map lis es');
         console.log(mapList);
-        
-        
+
+
         mapList.forEach(c => {
-            console.log('collection viene ');console.log(collectionUpdate);
-            
+            console.log('collection viene '); console.log(collectionUpdate);
+
             console.log('y va a entrar a ' + c);
-            
-            const flag = identificateVar(c, 'string', 'subCollection', 'imposible') 
+
+            const flag = identificateVar(c, 'string', 'subCollection', 'imposible')
 
             if (flag === 'string') {
                 collectionUpdate = collectionUpdate[c]
@@ -40,16 +40,16 @@ const EditModal = (props: modalProps) => {
 
             console.log('entro y quedo');
             console.log(collectionUpdate);
-            
-            
+
+
         })
         if (editAs === 'objectList') {
-           
+
             console.log('finalmente va a actualizar en');
             console.log(collectionUpdate);
-    
+
             collectionUpdate[indexEdit] = objeto
-           
+
         } else if (editAs === 'simpleList') {
             console.log('finalmente va a actualizar la lista');
             console.log(collectionUpdate);
@@ -58,8 +58,8 @@ const EditModal = (props: modalProps) => {
             });
             console.log('collectionUpdate');
             console.log(collectionUpdate);
-            
-            
+
+
         } else if (editAs === 'object') {
             console.log('finalmente va a actualizar el objeto');
             console.log(collectionUpdate);
@@ -72,18 +72,26 @@ const EditModal = (props: modalProps) => {
         console.log('actualizó');
         console.log('y el propierties es');
         console.log(doc.propierties);
-       
-        
-        
-        
-      editDoc(doc.id, doc); onClose()
+
+
+
+
+        editDoc(doc.id, doc); onClose()
     }
     const submitAdd = () => {
+        indexList.forEach((i) =>{
+            const identificate = identificateVar(obj[i], '', 'array', 'object');
+            if (identificate === 'array') {
+                objeto.propierties[i] = [];
+            } else if (identificate === 'object') {
+                objeto.propierties[i] = obj[i]
+            }
+        })
         console.log(objeto.propierties); addDoc(objeto); onClose()
     }
 
     const change = (e: React.ChangeEvent<HTMLInputElement>, i: string) => {
-        objeto[i] = e.target.value;
+        objeto.propierties[i] = e.target.value;
         setObject(objeto);
         setEdit(true)
     }
@@ -91,23 +99,23 @@ const EditModal = (props: modalProps) => {
     useEffect(() => {
         console.log('obj es');
         console.log(obj);
-        
-        
-      asEdit ?  setObject(obj) : setObject({id: 0, propierties: {}})
+
+
+        asEdit ? setObject(obj) : setObject({ id: 0, propierties: {} })
         return () => { };
     }, []);
 
     const modalButtons = () => {
         return <>
-         <div className="modal-buttons">
-                     <button type="button" onClick={() => asEdit ? submitEdit() : submitAdd()} className="submit-button">{asEdit ? 'Editar' : 'Crear'}</button>
-                     <button type="button" onClick={onClose} className="cancel-button">Cancel</button>
-                 </div>
+            <div className="modal-buttons">
+                <button type="button" onClick={() => asEdit ? submitEdit() : submitAdd()} className="submit-button">{asEdit ? 'Editar' : 'Crear'}</button>
+                <button type="button" onClick={onClose} className="cancel-button">Cancel</button>
+            </div>
         </>
     }
 
     return (
-      <div
+        <div
             className="modal-overlay"
             onClick={(e) => {
                 // Verifica si el clic fue en el overlay y no en el contenido del modal
@@ -117,35 +125,38 @@ const EditModal = (props: modalProps) => {
             }}
         >
             <div className="modal-content">
-              { editAs == 'simpleList'? <>
-                <SimpleTable edit={edit} setEdit={setEdit} setObject={setObject} title={mapList[mapList.length - 1]} obj={objeto ?? []}></SimpleTable>
-                {modalButtons()}
-              </> :  <>
-                 <h2>{mapList}</h2>
-                 {indexList.map((i) => <label>
-                    <div style={{width: '10%'}}>{i}</div> 
-                    <div style={{width: '10%'}}> <input type="text" value={objeto && !edit ? objeto[i] : undefined} onChange={(e) => change(e, i)} /></div>    
-                 </label>)}
-                {modalButtons()}
+                {editAs == 'simpleList' ? <>
+                    <SimpleTable edit={edit} setEdit={setEdit} setObject={setObject} title={mapList[mapList.length - 1]} obj={objeto ?? []}></SimpleTable>
+                    {modalButtons()}
+                </> : <>
+                    <h2>{mapList}</h2>
+                    {indexList.map((i) => {
+                        const render = identificateVar(asEdit? objeto[i] : obj[i], true, false, false);
+                       return render ? <label>
+                        <div style={{ width: '10%' }}>{i}</div>
+                        <div style={{ width: '10%' }}> <input type="text" value={objeto && !edit ? objeto[i] : undefined} onChange={(e) => change(e, i)} /></div>
+                    </label> : <></>})}
+                    {modalButtons()}
                 </>}
-               
+
             </div>
         </div>
     );
 };
 
-const SimpleTable = ({obj, title, setObject, edit, setEdit}: any) => {
-console.log('lista simple es');
-console.log(obj);
+const SimpleTable = ({ obj, title, setObject, edit, setEdit }: any) => {
+    console.log('lista simple es');
+    console.log(obj);
 
 
     return <>
-    <h2>{title}</h2>
-    {obj.map((i: string, index: number) => <label>
-                   <div style={{width: '10%'}}> <input type="text" value={!edit ?obj[index] : undefined} onChange={(e) => {obj[index] = e.target.value; setEdit(true); setObject(obj); console.log('actualiza'); console.log(obj);
-                   ;
-                   }} /></div>    
-                </label>)}
+        <h2>{title}</h2>
+        {obj.map((i: string, index: number) => <label>
+            <div style={{ width: '10%' }}> <input type="text" value={!edit ? obj[index] : undefined} onChange={(e) => {
+                obj[index] = e.target.value; setEdit(true); setObject(obj); console.log('actualiza'); console.log(obj);
+                ;
+            }} /></div>
+        </label>)}
     </>
 }
 
